923927b6cf64fd714e4d31d10d7146a4f7be93b0
Bram Wubs <bwubs@me.com>
2016-11-15 14:43:53 +0100

    Revert "Bug fixes"
    This reverts commit e1825a101709a9ea2bee8979b6425014c46a8c9e.

--- source/map.js
+++ source/map.js
@@ -1,32 +1,31 @@
 import {isArray} from "vue/src/util/lang";
 
 export function map(model, response) {
-    // console.log(response);
-    var resolved = (isArray(response))
-        ? mapResponseToMultiple(response, model)
-        : mapResponseToModel(response, model);
-    return Promise.resolve(resolved);
+  var resolved = (isArray(response))
+    ? mapResponseToMultiple(response, model)
+    : mapResponseToModel(response, model);
+  return Promise.resolve(resolved);
 }
 
 function mapResponseToModel(response, bluePrint) {
-    var model = (typeof bluePrint === 'function') ? new bluePrint : bluePrint;
-    for (var key in response) {
-        if (response.hasOwnProperty(key)) {
-            model.properties[key] = response[key];
-            model.originals[key] = response[key];
-            //make sure relations aren't set directly on
-            //the model.
-            if (typeof model[key] === 'undefined') {
-                model[key] = response[key];
-            }
+  var model = (typeof bluePrint === 'function') ? new bluePrint : bluePrint;
+  for (var key in response) {
+    if (response.hasOwnProperty(key)) {
+      model.properties[key] = response[key];
+      model.originals[key] = response[key];
+      //make sure relations aren't set directly on
+      //the model.
+      if (typeof model[key] === 'undefined') {
+        model[key] = response[key];
+      }
 
-        }
     }
-    return model;
+  }
+  return model;
 }
 
 function mapResponseToMultiple(response, model) {
-    return response.map(function (item) {
-        return mapResponseToModel(item, model);
-    });
+  return response.map(function (item) {
+    return mapResponseToModel(item, model);
+  });
 }
